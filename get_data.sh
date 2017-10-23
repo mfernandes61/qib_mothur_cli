61849636432b2f7dcbd4f5c922fc7e153f03f961
Mark Fernandes <mfernandes61@gmail.com>
2017-10-23 16:23:09 +0100

    Update get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # script to pull data for Mothur tutorial from Zenodo
 cd /home
-mkdir /data /reference
-wget https://zenodo.org/record/165147/files/input_data.zip data
-wget https://zenodo.org/record/165147/files/reference_data.zip reference
-unzip data/*.zip
-unzip reference/*.zip
+# mkdir /data /reference
+wget https://zenodo.org/record/165147/files/input_data.zip 
+wget https://zenodo.org/record/165147/files/reference_data.zip 
+unzip *.zip
+#unzip reference/*.zip
