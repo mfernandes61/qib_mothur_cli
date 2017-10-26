48a5ae3e7d3c00884eb279ebd50e7b94f4f84cf6
Mark Fernandes <mfernandes61@gmail.com>
2017-10-26 10:54:23 +0100

    Update get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -14,6 +14,8 @@ unzip *.zip
 rm *.zip
 cd /home/horse
 wget http://hpc.ilri.cgiar.org/beca/training/AdvancedBFX2015/metagenomics/data.zip
+wget http://www.mothur.org/w/images/9/98/Silva.bacteria.zip
+wget -N http://mothur.org/w/images/5/59/Trainset9_032012.pds.zip
 wget http://hpc.ilri.cgiar.org/beca/training/AdvancedBFX2015/metagenomics/Hands-on_16S_rRNA_gene_Metagenomics-Mothur_tutorial.pdf
 unzip *.zip
 rm *.zip
